3f5799282fee1d5ce7630c0b8af0c93e3a12cfc1
KURASAWA Nozomu <nabetaro@caldron.jp>
2016-10-26 19:03:00 +0900

    検出ロジックの高速化と表示の遅延対策

--- openvc-camera.rb
+++ openvc-camera.rb
@@ -4,19 +4,17 @@
 require 'opencv'
 include OpenCV
 
-# マシンに接続されているカメラのうち, 1番めのカメラを利用する
 camera = CvCapture.open(0)
 window = GUI::Window.new('camera')
+detector = CvHaarClassifierCascade::load '/usr/share/opencv/haarcascades/haarcascade_profileface.xml'
 
-# このループ内で何かする
-# 今回はただ表示するだけ
 while true
+
   # Iplimageクラスの画像データをキャプチャする
   image = camera.query
-
-  detector = CvHaarClassifierCascade::load '/usr/share/opencv/haarcascades/haarcascade_profileface.xml'
+  now = Time.now
+  puts "#{now}.#{now.usec}:"
   detector.detect_objects(image).each do |region|
-    now = Time.now
     puts "#{now}.#{now.usec}: (#{region.top_left.x}, #{region.top_left.y}, #{region.bottom_right.x}, #{region.bottom_right.y})"
     color = CvColor::Blue
     image.rectangle! region.top_left, region.bottom_right, :color => color
@@ -24,8 +22,9 @@ while true
 
   window.show image
 
-  key = GUI::wait_key 10
+  key = GUI::wait_key 1
 
   # キーボードでqを入力したら終了
   break if key == 113
+
 end
